12f939c052b46d4ea02b5383d15aac4d2e835c24
Alex Ragalie <alex.ragalie@gmail.com>
2018-03-29 12:17:18 +0200

    Update scikitimage_Project.py

--- scikitimage_Project.py
+++ scikitimage_Project.py
@@ -7,15 +7,13 @@
 
 OBJECTIVE
 
-The task is to use a 5x5 window to scan over the greyscale image, where a pixel value is a float in the range 0 to 1.
+Use a 5x5 window to scan over the greyscale image, where a pixel value is a float in the range 0 to 1.
 
 The values have to be normalised so that within one window the smallest value is scaled to 0 and the largest to 1.
 
-scikit-image must be used.
+OUTPUT
 
-EXPECTED OUTPUT
-
-Export data to .csv, in a table with 25 columns and 13,824 rows.
+Data export to .csv, in a table with 25 columns and 13,824 rows.
 
 """
 
